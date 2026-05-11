fd04058991c1003ec4ef822a83a120178bcf4637
CHAKIR Zakariya <12403275@t20620.iutv.univ-paris13.fr>
2026-05-11 11:37:02 +0200

    add: methods prixMoyen_avecDeuxVoitures_retourneLaMoyenne(),  prixMoyen_avecUneVoiture_retourneLaMoyenne()

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -2,16 +2,37 @@ package com.example.demo.service;
 
 import com.example.demo.data.Voiture;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-
-@SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    @Test
+    public void prixMoyen_avecDeuxVoitures_retourneLaMoyenne() {
+        Voiture mockV1 = mock(Voiture.class);
+        Voiture mockV2 = mock(Voiture.class);
 
+        when(mockV1.getPrix()).thenReturn(10000);
+        when(mockV2.getPrix()).thenReturn(5000);
+
+        StatistiqueImpl stats = new StatistiqueImpl();
+
+        stats.ajouter(mockV1);
+        stats.ajouter(mockV2);
+
+        assertEquals(7500,stats.prixMoyen());
+        }
+    
+    @Test
+    public void prixMoyen_avecUneVoiture_retourneLaMoyenne() {
+        Voiture mockV1 = mock(Voiture.class);
+        when(mockV1.getPrix()).thenReturn(10000);
+
+        StatistiqueImpl stats = new StatistiqueImpl();
+
+        stats.ajouter(mockV1);
+
+        assertEquals(10000, stats.prixMoyen());
+    }
 }
