712503f4655b8779e35ce4affe3e1a61b642e7a2
zChakir954 <12403275@t20620.iutv.univ-paris13.fr>
2026-05-11 14:32:37 +0200

    add: test des autres endpoints

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -38,4 +38,28 @@ class WebTests {
                 .andExpect(jsonPath("$.prixMoyen").value(7500));
     }
 
+    @Test
+    public void getStatistiques_sansVoiture_retourneErreur() throws Exception {
+        when(statistiqueImpl.prixMoyen())
+            .thenThrow(new ArithmeticException);
+
+        mockMvc.perform(get("/statistique"))
+            .andDo(print())
+            .andExpect(status().isNotFound());
+    }
+
+     @Test
+    public void creerVoiture_avecJsonValide_appelleAjouter() throws Exception {
+
+        String voitureJson = "{\"marque\":\"Audi\",\"prix\":10000}";
+
+        mockMvc.perform(post("/voiture")
+                .contentType("application/json")
+                .content(voitureJson))
+               .andDo(print())
+               .andExpect(status().isOk());                             
+
+        verify(statistiqueImpl, times(1)).ajouter(any());
+    }
+
 }
